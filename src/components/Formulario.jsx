dccdc21086576bba216ecd7799d1f16deb0a0f6d
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-13 12:10:02 -0600

    Feat. Save patients

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,12 +7,12 @@ export const Formulario = () => {
     const [nombre, setNombre] = useState('');
     const [propietario, setPropietario] = useState('');
     const [email, setEmail] = useState('');
-    const [fecha, setFecha] = useState(Date.now());
+    const [fecha, setFecha] = useState('');
     const [sintomas, setSintomas] = useState('');
 
     const [alerta, setAlerta] = useState({});
 
-    const { pacientes } = usePacientes();
+    const { guardarPaciente } = usePacientes();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -24,6 +24,9 @@ export const Formulario = () => {
                 error: true,
             })
         }
+
+        setAlerta({});
+        guardarPaciente({ nombre, propietario, email, fecha, sintomas });
     };
 
     const { msg } = alerta;
--- src/context/PacientesProvider.jsx
+++ src/context/PacientesProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from "react";
+import clienteAxios from "../config/axios";
 
 export const PacientesContext = createContext({});
 
@@ -6,9 +7,56 @@ export const PacientesProvider = ({ children }) => {
 
     const [pacientes, setPacientes] = useState([]);
 
+    useEffect(() => {
+        const obtenerPacientes = async () => {
+            try {
+                const token = localStorage.getItem('token');
+
+                if (!token) return;
+
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    }
+                };
+
+                const { data } = await clienteAxios('/pacientes', config);
+
+                setPacientes(data);
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        obtenerPacientes();
+    }, [])
+
+    const guardarPaciente = async (paciente) => {
+        try {
+            const token = localStorage.getItem('token');
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                }
+            }
+
+            const { data } = await clienteAxios.post('/pacientes', paciente, config);
+
+            const { createdAt, updatedAt, __v, ...pacienteAlmacenado } = data;
+
+            setPacientes([pacienteAlmacenado, ...pacientes]);
+        } catch (error) {
+            console.log(error.response.data.msg);
+        }
+    }
+
     return (
         <PacientesContext.Provider value={{
             pacientes,
+            guardarPaciente
         }}>
             {children}
         </PacientesContext.Provider>
